50ddc0a3de28f28c2ad08eef4e05f17b938ed394
cdewansh <cdewansh@gmail.com>
2023-11-02 12:32:15 +0530

    Comments added

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ app = Flask(__name__,template_folder="templates")
 
 todos = [{'todo':"Sample todo", "Done": False}]
 
+""" '@' decorator function in Python
+use: defines a route in a Flask application, and when a user accesses the root URL, 
+it calls the index function, which renders an HTML template and passes a variable (todos) 
+to that template, which can be used to display dynamic data in the web page"""
 @app.route('/')
 def index():
     return render_template("index.html", todos = todos)
 
+
+# Code is executed only when file is excuted as main file.
 if __name__ == "__main__":
     app.run(debug=True)
